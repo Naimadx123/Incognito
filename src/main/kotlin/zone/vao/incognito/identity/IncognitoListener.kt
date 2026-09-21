a08e5bec9be3c1f8a804212fc8f292d151de9e52
Naimadx123 <damianx787@gmail.com>
2026-09-21 12:35:24 +0200

    feat: Add player name masking for server ping events

--- src/main/kotlin/zone/vao/incognito/identity/IncognitoListener.kt
+++ src/main/kotlin/zone/vao/incognito/identity/IncognitoListener.kt
@@ -1,5 +1,6 @@
 package zone.vao.incognito.identity
 
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -11,6 +12,14 @@ class IncognitoListener(private val service: IncognitoService) : Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     fun onJoin(event: PlayerJoinEvent) {
         service.load(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onPing(event: PaperServerListPingEvent) {
+        if (!service.settings.names) return
+        event.listedPlayers.replaceAll { listed ->
+            service.identity(listed.id())?.let { PaperServerListPingEvent.ListedPlayerInfo(it.alias, it.id) } ?: listed
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
